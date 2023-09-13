6134a69098196b25c4b57d1d92b80a993316bc6c
Maurine-Jebet <maurinejebett@gmail.com>
2023-09-13 20:49:19 +0300

    commented workflow files

--- Amali/donation/models.py
+++ Amali/donation/models.py
@@ -1,13 +1,14 @@
-
+from django.contrib import admin
 from django.db import models
 
 # Create your models here.
 class Donation(models.Model):
+    
     amount = models.DecimalField(max_digits=10, decimal_places=2)
    
     def __str__(self):
         return f" Amount: {self.amount}"  
-    
-       
+      
     class Meta:
-        verbose_name_plural = "Donation"        
+        verbose_name_plural= "Donations"
+
