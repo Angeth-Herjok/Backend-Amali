f7db34d4675bcc0084777564d40793232086f7b9
Maurine-Jebet <maurinejebett@gmail.com>
2023-09-13 21:37:23 +0300

    put comments

--- Amali/donation/models.py
+++ Amali/donation/models.py
@@ -5,9 +5,12 @@ from django.db import models
 class Donation(models.Model):
     
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    # sponsor = models.ForeignKey(Sponsor, on_delete=models.CASCADE)
+    # athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
    
     def __str__(self):
         return f" Amount: {self.amount}"  
+        # Donation from {self.sponsor.Name} to {self.athlete.name} 
       
     class Meta:
         verbose_name_plural= "Donations"
