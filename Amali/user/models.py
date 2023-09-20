2c8b8785b3cde0fa8615a6282ff6f8c866a139ee
chepkemboirabecca@gmail.com <chepkemboirabecca@gmail.com>
2023-09-20 09:23:58 +0300

    added login functionality

--- Amali/user/models.py
+++ Amali/user/models.py
@@ -1,31 +1,3 @@
-# from django.contrib.auth.models import AbstractUser
-# from django.db import models
-# from phonenumber_field.modelfields import PhoneNumberField
-
-# class CustomUser(AbstractUser):
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=100)
-#     phone_number = PhoneNumberField(unique=True, blank=True, null=True)
-#     password = models.CharField(max_length=128)
-#     confirm_password = models.CharField(max_length=128,null=True)
-#     groups = models.ManyToManyField(
-#         'auth.Group',
-#         related_name='customuser_set',
-#         blank=True,
-#         help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-#         verbose_name='groups',
-#     )
-#     user_permissions = models.ManyToManyField(
-#         'auth.Permission',
-#         related_name='customuser_set',
-#         blank=True,
-#         help_text='Specific permissions for this user.',
-#         verbose_name='user permissions',
-#     )
-
-
-
-
 from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 from django.utils.translation import gettext as _
@@ -39,7 +11,6 @@ class CustomUser(AbstractUser):
     )
     role = models.CharField(_('Role'), max_length=15, choices=USER_ROLES, default='regular_user')
     email = models.EmailField(unique=True)
-    # first_name = models.CharField(max_length=100)
     phone_number = PhoneNumberField(_('Phone Number'), blank=True, null=True)
     password = models.CharField(max_length=128)
     confirm_password = models.CharField(max_length=128, null=True)
@@ -60,22 +31,6 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
-
-# class Athlete(models.Model):
-#     # user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
-#     # age = models.PositiveIntegerField()
-#     GENDER_CHOICES = [
-#         ('male', 'Male'),
-#         ('female', 'Female'),
-#     ]
-#     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='')
-#     # profile_picture = models.ImageField(upload_to='profile_pictures/')
-#     # achievements = models.TextField()
-#     password = models.CharField(max_length=128, null=True)
-#     confirm_password = models.CharField(max_length=128, null=True)
-
-#     def __str__(self):
-#         return self.email
 
 
     
