0391643eb2dda8a39927e9926510822a01354fd0
Angeth-Herjok <angethherjok@gmail.com>
2023-10-02 21:35:27 +0300

    added users

--- donation/models.py
+++ donation/models.py
@@ -9,6 +9,7 @@ class Donation(models.Model):
     athlete=models.OneToOneField(Athlete,on_delete=models.CASCADE,default="")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     sponsors = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name='sponsored_donations',default="")
+    
 
     def __str__(self):
         return f"Amount: {self.amount} Donation from {self.sponsors} to {self.athlete}"
