15eeb25eb8495d36e69a326fc33a4789fcdce656
Angeth-Herjok <angethherjok@gmail.com>
2023-10-03 08:52:56 +0300

    added feature/users

--- api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Comment
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'comment', 'likes')
-    search_fields = ('full_name', 'comment')
-    list_filter = ('likes',)
-    readonly_fields = ('likes',)
