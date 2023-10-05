b25b69845b48d62c7adbd03545963d6fbada652d
Angeth-Herjok <angethherjok@gmail.com>
2023-10-05 10:47:36 +0300

    fix bugs

--- donation/models.py
+++ donation/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from register.models import Sponsor, Athlete
 
-class Donation(models.Model):
+class Donation(models, model):
     athlete = models.OneToOneField(Athlete, on_delete=models.CASCADE, default="")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     sponsors = models.ForeignKey(Sponsor, on_delete=models.CASCADE, related_name='sponsored_donations', default="")
