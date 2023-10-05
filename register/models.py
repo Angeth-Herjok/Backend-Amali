930437c6dd44998c99f8af31d6ce2418b47e8344
Angeth-Herjok <angethherjok@gmail.com>
2023-10-05 13:25:15 +0300

    fix bugs

--- register/models.py
+++ register/models.py
@@ -58,3 +58,5 @@ class Sponsor(models.Model):
     Bio = models.TextField()
     phone_number = PhoneNumberField( blank=True, null=False, default= None)
     role = models.CharField(max_length=20, default='sponsor')
+
+    # models
